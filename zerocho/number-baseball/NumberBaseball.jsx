85cc9d0a07713ad7f0f8c7e8e14257dfba31b5b1
PSH <pshysd@kakao.com>
2023-06-30 00:23:33 +0900

    [FEAT] 숫자야구 클래스

--- zerocho/number-baseball/NumberBaseball.jsx
+++ zerocho/number-baseball/NumberBaseball.jsx
@@ -1,7 +1,13 @@
 import { Component } from 'react';
 
 const getNumbers = () => {
-
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
 };
 
 class NumberBaseball extends Component {
@@ -12,7 +18,50 @@ class NumberBaseball extends Component {
     attempts: [],
   };
 
-  onSubmitForm = () => {};
+  onSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (this.state.value === this.state.answer.join('')) {
+      this.setState({
+        result: '홈런!',
+        attempts: [...this.state.attempts, { attempt: this.state.value, result: '홈런!' }]
+      });
+
+      alert('게임을 다시 시작합니다.');
+      this.setState({
+        value: '',
+        answer: getNumbers(),
+        attempts: [],
+      });
+    } else {
+      const answerArray = this.state.value.split('').map(v => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (this.state.attempts.length >= 9) {
+        this.setState({
+          result: `졋음ㅉㅉ 정답알려줌 ${answer.join(',')}`
+        });
+        alert('게임을 다시 시작합니다');
+        this.setState({
+          value: '',
+          answer: getNumbers(),
+          attempts: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === this.state.answer[i]) {
+            strike += 1;
+          } else if (this.state.answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState({
+          attempts: [...this.state.attempts, { attempt: this.state.value, result: `${strike} 스트라이크, ${ball} 볼` }],
+          value: '',
+        });
+      }
+    }
+  };
 
   onChangeInput = () => {};
 
